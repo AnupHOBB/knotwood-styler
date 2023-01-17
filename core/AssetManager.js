043c986bbabaa43d5636a83deab9cef3ca6082d3
Anup Bhandari <Anup.b@HOBB.COM>
2023-01-17 17:53:24 +0530

    Documented and refactored code

--- core/AssetManager.js
+++ core/AssetManager.js
@@ -1,22 +1,22 @@
 import { toImageDataFromImage } from './Helpers.js'
 import {LayerManager} from "./LayerManager.js"
+
 /**
- * This class downloads all the images and stores them as image data within the assetMap variable.
- * The assetMap consists of entries where : (key : layer name i.e. glass/bath/floor/base) and (value : setMap).
- * The setMap consists of entries where : (key : set name i.e mobile/pc) and (value: array of imagedata of a particular layer).
- * The variable screenTypes refers to the name for various screen sizes i.e. mobile, pc, etc.
- * The variable layerNames refers to the name for various images i.e. base, floor, glass, bath etc. This is just an array of the names of image layers.
+ * This class is just a wrapper around the AssetManagerCore class that calls the same functions within AssetManagerCore.
+ * Refer to the AssetManagerCore class for detail description of each function.
+ * This was done to hide the functions within AssetManagerCore that are meant to be private.
+ * Use this to manage all the assets and image layers. 
  */
 export class AssetManager
 {
-    constructor(screenTypes, layerNames, assetPathMap, icons)
+    constructor(screenTypes, layerNames, assetPathMap)
     {
-        this.core = new AssetManagerCore(screenTypes, layerNames, assetPathMap, icons)
+        this.core = new AssetManagerCore(screenTypes, layerNames, assetPathMap)
     }
 
-    loadAssets(onDownloadComplete)
+    downloadAssets(onDownloadComplete)
     {
-        this.core.loadAssets(onDownloadComplete)
+        this.core.downloadAssets(onDownloadComplete)
     }
 
     changeColor(layerName, colorKeyName)
@@ -55,28 +55,150 @@ export class AssetManager
     }
 }
 
+/**
+ * This is the core class responsible for downloading and managing the assets and icons.
+ */
 class AssetManagerCore
 {    
-    constructor(screenTypes, layerNames, assetPathMap, icons)
+    /**
+     * @param {Array} screenTypes contains array of string that holds screen names. Screen name or Screen type name is the name of the screen for which there will be a variant of an asset 
+     * present. Every image layer will have different variant based on the screen sizes. E.g : ['mobile', 'pc'] in this case. This was added to allow support for multiple screen sizes. 
+     * Any name for screen type can be given. But folders with those names must be present inside the assets folder within the project as these names will be used to fetch assets. 
+     * 
+     * @param {Array} layerNames contains array of string that holds layer names. Layer names are the name of image layers that will be stacked on top of another when displaying the final
+     * combined image. Layering is done to allow images to be separately modified. In this project, for a bathroom image, separate layers of glass, floor, bath and a base backgorund images
+     * were used, combined and drawn into a canvas for displaying.  E.g : ['glass', 'floor', 'bath', 'base'] in this case. Any name for layers can be given. But folders with those names must 
+     * be present inside the "assets/[screnType]/" folder within the project as these names will be used to fetch assets. In case of this project, the layer folders are present inside 
+     * "assets/mobile/" and "assets/pc/" directories as this project has "mobile" and "pc" as screen types.
+     * 
+     * @param {Map} assetPathMap contains the relative path of all the color, texture and icon assets. The program assumes that all the color and texture assets will be stored inside 
+     * "assets/[screen type]/[layer name]/"\and icons inside assets/icon/[layer name]/ folders. In case of this project, the directories where the colors and textures of glass layer 
+     * are stored are :-"assets/mobile/glass/colors/","assets/mobile/glass/textures/" and icons are stored inside assets/icons/glass folders.
+     * 
+     * And the JSON structure of the assetPathMap is as follows :-
+     *  [
+     *       {
+     *           "key" : "layerName",
+     *           "value" :
+     *           [
+     *               {
+     *                   "0": 
+     *                   [//This is colorMap
+     *                       {
+     *                           "key": "colorKeyName",
+     *                           "value": "color path"
+     *                       }
+     *                   ]
+     *               },
+     *               {
+     *                   "1": 
+     *                   [//This is textureMap
+     *                       {
+     *                           "key": "textureKeyName",
+     *                           "value": "texture path"
+     *                       }
+     *                   ]
+     *               },
+     *               {
+     *                   "2": 
+     *                   [//This is iconMap
+     *                      {
+     *                          "key": "iconKeyName",
+     *                          "value": "icon path"
+     *                      }
+     *                   ]
+     *               }
+     *           ]
+     *       }
+     *  ]
+     * 
+     * Here, the color path should start from within the layer folder. In this case, value will be given as "colors/0.png".
+     * Also for the texture path, value should start from within the layer folder. In this case, value will be given as "textures/0.png".
+     * But for icon path, value should start from within the assets folder. In this case, value will be given as "[icon-name].png". 
+     * E.g : for floor icons path will be "0.png". This is the image used as floor icon here.
+     * 
+     * The path of colors, textures and icons SHOULD NOT start from the asset folder. It should start from within the layer folder for colors and textures 
+     * and from within the layer folder inside icon folder for icons.
+     * 
+     * After downloading the assets, the assets will be stored in the assetMap variable in the JSON format as shown below :-
+     * [
+     *     {
+     *           "key" : "layername",
+     *           "value" : 
+     *           [//This is an array that stores screenMap at index 0 and iconMap at index 1
+     *               {
+     *                   "0":
+     *                   [// This is screenMap
+     *                       {
+     *                           "key" : "screenType",
+     *                           "value" : 
+     *                          [
+     *                               {
+     *                                   "0": 
+     *                                   [
+     *                                       {//This is colorMap
+     *                                           "key": "colorKeyName",
+     *                                           "value": ImageData object 
+     *                                       }
+     *                                   ]
+     *                               },
+     *                               {
+     *                                   "1": 
+     *                                   [
+     *                                       {//This is textureMap
+     *                                           "key": "textureKeyName",
+     *                                           "value": ImageData object
+     *                                       }
+     *                                   ]
+     *                               }
+     *                           ]
+     *                       }
+     *                   ]
+     *               },
+     *               {
+     *                   "1":
+     *                   [// This is iconMap
+     *                       {
+     *                           "key" : "iconKey",
+     *                           "value": ImageData object
+     *                       }
+     *                   ]
+     *               }
+     *           ]
+     *       }
+     *  ]
+     *  The assetMap instance variable stores all the downloaded assets as imageData.
+     */
+    constructor(screenTypes, layerNames, assetPathMap)
     {
-        this.mainFolderPath = '../assets/'
-        this.iconsPath = this.mainFolderPath + 'icons/'
-        this.screenTypes = screenTypes
-        this.layerNames = layerNames
-        this.assetMap = new Map()
-        this.assetCounter = 0
-        this.layerManager = new LayerManager(screenTypes, layerNames)
-        this.pathMap = assetPathMap
-        this.icons = icons
-        this.defaultTextures = []
-        this.assetTypeColor = 'color'
+        this.mainFolderPath = '../assets/' //path to root folder
+        this.iconsPath = this.mainFolderPath + 'icons/' //path to icons folder
+        this.screenTypes = screenTypes //array of screenType values
+        this.layerNames = layerNames //array of layer names
+        this.assetCounter = 0  //counter used for counting the downloaded assets. If assetCounter == totalAssets, then a callback function is called to notify user of completion.
+        this.layerManager = new LayerManager(screenTypes, layerNames) // object that is responsible for managing image layers for multiple screen sizes.
+        this.assetPathMap = assetPathMap //stores map that contains asset paths
+        this.defaultTextures = [] //stores the key value of all the default textures for each image layer
+        
+        /**
+         * The following three values are used to distinguish between assets.
+         * Once an asset has been download, the asset along with one of the following values will be passed along the onDownload function below for storage.
+         */
+        this.assetTypeColor = 'color'  
         this.assetTypeTexture = 'texture'
         this.assetTypeIcon = 'icon'
-        this.totalAssets = this.getTotalAssetCount()
-        this.iconMap = new Map()
+
+        this.totalAssets = this.getTotalAssetCount() //value that stores the number of all assets
+        this.assetMap = new Map() //stores all the downloaded assets.
         this.setupAssetMap()
     }
 
+    /**
+     * Changes the color of the layer for a specific screen whose name is given.
+     * @param {string} screenType name of the screen type.
+     * @param {string} layerName name of the image layer.
+     * @param {string} colorKeyName name of the key of the color asset that was provided within the color map inside the assetPathMap.
+     */
     changeColor(screenType, layerName, colorKeyName)
     {
         let assets = this.assetMap.get(layerName)
@@ -87,6 +209,12 @@ class AssetManagerCore
         this.layerManager.changeColor(screenType, layerName, colorImageData)
     }
 
+    /**
+     * Replaces the texture of the layer for a specific screen  whose name is given.
+     * @param {string} screenType name of the screen type.
+     * @param {string} layerName name of the image layer.
+     * @param {string} textureKeyName name of the key of the texture asset that was provided within the texture map inside the assetPathMap.
+     */
     changeLayer(screenType, layerName, textureKeyName)
     {
         let assets = this.assetMap.get(layerName)
@@ -97,6 +225,11 @@ class AssetManagerCore
         this.layerManager.changeLayer(screenType, layerName, newImageData)
     }
     
+    /**
+     * Changes the color of the layer for all screen type whose name is given.
+     * @param {string} layerName name of the image layer
+     * @param {string} colorKeyName key for the color asset that was included in the assetPathMap
+     */
     changeColorForAllScreens(layerName, colorKeyName)
     {
         let assets = this.assetMap.get(layerName)
@@ -110,6 +243,11 @@ class AssetManagerCore
         }
     }
 
+   /**
+     * Replaces the texture of the layer for all screen type  whose name is given.
+     * @param {string} layerName name of the image layer
+     * @param {string} textureKeyName key for the texture asset that was included in the assetPathMap
+     */
     changeLayerForAllScreens(layerName, textureKeyName)
     {
         let assets = this.assetMap.get(layerName)
@@ -123,11 +261,22 @@ class AssetManagerCore
         }
     }
 
+    /**
+     * Draws the layers onto the canvas.
+     * @param {string} screenType name of the type of screen onto which the layers need to be displayed
+     * @param {Canvas} canvas where the layers will be displayed
+     */
     drawOnCanvas(screenType, canvas)
     {
         this.layerManager.drawOnCanvas(screenType, canvas)
     }
 
+    /**
+     * Retrieves the color icons for a specific layer.
+     * For colors, the values within the colorMap inside assetMap is retrieved.
+     * @param {string} layerName name of the image layer
+     * @returns colorMap
+     */
     getColorIcons(layerName)
     {
         let screenType = this.screenTypes[0]
@@ -136,19 +285,33 @@ class AssetManagerCore
         return maps[0]
     }
 
+   /**
+     * Retrieves the texture icons for a specific layer.
+     * For textures, the values within the iconMap inside assetMap is retrieved.
+     * @param {string} layerName name of the image layer
+     * @returns iconMap as Map
+     */
     getImageIcons(layerName)
     {
         let assets = this.assetMap.get(layerName)
         return assets[1]
     }
 
+    /**
+     * All functions from this point on are meant to be private.
+     */
+
+    /**
+     * Returns the number of total assets present within assetPathMap.
+     * @returns total asset count as int
+     */
     getTotalAssetCount()
     {
         let assetCounter = 0
-        let layers = this.pathMap.keys()
+        let layers = this.assetPathMap.keys()
         for (let layer of layers)
         {
-            let assets = this.pathMap.get(layer)
+            let assets = this.assetPathMap.get(layer)
             for (let i=0; i<assets.length; i++)
             {
                 let map = assets[i]
@@ -158,6 +321,9 @@ class AssetManagerCore
         return assetCounter * this.screenTypes.length
     }
 
+    /**
+     * Initializes the assetMap variable
+     */
     setupAssetMap()
     {
         for (let j=0; j<this.layerNames.length; j++)
@@ -174,19 +340,23 @@ class AssetManagerCore
         }
     }
 
-    loadAssets(onDownloadComplete)
+    /**
+     * Downloads all the assets
+     * @param {Function} onDownloadComplete callback function that is called after all assets are downloaded  
+     */
+    downloadAssets(onDownloadComplete)
     {
         for (let i=0; i<this.screenTypes.length; i++)
         {
             for (let j=0; j<this.layerNames.length; j++)
             {
-                let assetPaths = this.pathMap.get(this.layerNames[j])
+                let assetPaths = this.assetPathMap.get(this.layerNames[j])
                 let colorMap = assetPaths[0]
                 let colorkeys = colorMap.keys()
                 for (let colorKey of colorkeys)
                 {   
                     const path = this.mainFolderPath + this.screenTypes[i] + '/' + this.layerNames[j] + '/' + colorMap.get(colorKey)
-                    this.downloadAssets(path, (imageData)=>this.onDownload(this.layerNames[j], this.screenTypes[i], imageData, onDownloadComplete, this.assetTypeColor, colorKey))
+                    this.downloadAsset(path, (imageData)=>this.onDownload(this.layerNames[j], this.screenTypes[i], imageData, onDownloadComplete, this.assetTypeColor, colorKey))
                 }
                 let textureMap = assetPaths[1]
                 let textureKeys = textureMap.keys()
@@ -195,20 +365,25 @@ class AssetManagerCore
                     if (this.defaultTextures[j] == undefined)
                         this.defaultTextures[j] = textureKey
                     const path = this.mainFolderPath + this.screenTypes[i] + '/' + this.layerNames[j] + '/' + textureMap.get(textureKey)
-                    this.downloadAssets(path, (imageData)=>this.onDownload(this.layerNames[j], this.screenTypes[i], imageData, onDownloadComplete, this.assetTypeTexture, textureKey))
+                    this.downloadAsset(path, (imageData)=>this.onDownload(this.layerNames[j], this.screenTypes[i], imageData, onDownloadComplete, this.assetTypeTexture, textureKey))
                 }
                 let iconMap = assetPaths[2]
                 let iconKeys = iconMap.keys()
                 for (let iconKey of iconKeys)
                 {
                     const path = this.iconsPath + this.layerNames[j] + '/' + iconMap.get(iconKey)
-                    this.downloadAssets(path, (imageData)=>this.onDownload(this.layerNames[j], this.screenTypes[i], imageData, onDownloadComplete, this.assetTypeIcon, iconKey))
+                    this.downloadAsset(path, (imageData)=>this.onDownload(this.layerNames[j], this.screenTypes[i], imageData, onDownloadComplete, this.assetTypeIcon, iconKey))
                 }
             }
         }
     }
 
-    downloadAssets(path, onDownload)
+    /**
+     * 
+     * @param {string} path url of the asset
+     * @param {Function} onAssetDownload callback function that is called after current asset is downloaded  
+     */
+    downloadAsset(path, onAssetDownload)
     {
         let reader = new XMLHttpRequest()
         reader.open('GET', path)
@@ -222,7 +397,7 @@ class AssetManagerCore
                 img.onload = () => 
                 {
                     URL.revokeObjectURL(bloburl)
-                    onDownload(toImageDataFromImage(img))
+                    onAssetDownload(toImageDataFromImage(img))
                 }
             }
         }
@@ -230,9 +405,18 @@ class AssetManagerCore
         reader.send()
     }
 
+    /**
+     * Callback function called after an asset is downloaded
+     * @param {string} layer name of the layer for to which this asset belongs to
+     * @param {string} screenType name of the screenType in which this asset will be displayed on selection.
+     * @param {ImageData} imageData asset as ImageData 
+     * @param {Function} onDownloadComplete callback to notify to in case all assets are downloaded
+     * @param {string} assetType will have one of the values between 'color', 'texture' and 'icon'. It is used to distinguish between the assets.
+     * @param {string} key key name of assets which was included in the assetPathMap.
+     */
     onDownload(layer, screenType, imageData, onDownloadComplete, assetType, key)
     {
-        this.storeAssets(layer, screenType, imageData, assetType, key)
+        this.storeAsset(layer, screenType, imageData, assetType, key)
         this.assetCounter++
         if (this.assetCounter == this.totalAssets)
         {
@@ -251,7 +435,15 @@ class AssetManagerCore
         }
     }
 
-    storeAssets(layer, screenType, imageData, assetType, key)
+    /**
+     * Stores the asset inside the assetMap
+     * @param {string} layer name of the layer for to which this asset belongs to
+     * @param {string} screenType name of the screenType in which this asset will be displayed on selection.
+     * @param {ImageData} imageData asset as ImageData 
+     * @param {string} assetType will have one of the values between 'color', 'texture' and 'icon'. It is used to distinguish between the assets.
+     * @param {string} key key name of assets which was included in the assetPathMap.
+     */
+    storeAsset(layer, screenType, imageData, assetType, key)
     {
         let assets = this.assetMap.get(layer)
         let screenMap = assets[0]
--- core/LayerManager.js
+++ core/LayerManager.js
@@ -1,7 +1,23 @@
 import { addColor, multiplyColor, duplicateImagedata } from './Helpers.js'
 
+/**
+ * This class manages the layers of images for different screen types.
+ */
 export class LayerManager
 {
+    /**
+     * 
+     * @param {Array} screenTypes contains array of string that holds screen names. Screen name or Screen type name is the name of the screen for which there will be a variant of an asset 
+     * present. Every image layer will have different variant based on the screen sizes. E.g : ['mobile', 'pc'] in this case. This was added to allow support for multiple screen sizes. 
+     * Any name for screen type can be given. But folders with those names must be present inside the assets folder within the project as these names will be used to fetch assets. 
+     * 
+     * @param {Array} layerNames contains array of string that holds layer names. Layer names are the name of image layers that will be stacked on top of another when displaying the final
+     * combined image. Layering is done to allow images to be separately modified. In this project, for a bathroom image, separate layers of glass, floor, bath and a base backgorund images
+     * were used, combined and drawn into a canvas for displaying.  E.g : ['glass', 'floor', 'bath', 'base'] in this case. Any name for layers can be given. But folders with those names must 
+     * be present inside the "assets/[screnType]/" folder within the project as these names will be used to fetch assets. In case of this project, the layer folders are present inside 
+     * "assets/mobile/" and "assets/pc/" directories as this project has "mobile" and "pc" as screen types.
+     * 
+     */
     constructor(screenTypes, layerNames)
     {
         this.layerManagerMap = new Map()
@@ -9,24 +25,47 @@ export class LayerManager
             this.layerManagerMap.set(screenTypes[i], new LayerRenderer(layerNames))
     }
 
+    /**
+     * Adds image layers as imagedata within the layerRenderer. Before modifying the layers, the layers need to be present within the LayerRenderer.
+     * @param {string} screenType is name of the screen type.
+     * @param {string} layerName is name of the image layer. 
+     * @param {ImageData} imageData image layer as ImageData
+     */
     addLayerAt(screenType, layerName, imageData)
     {
         let layerRenderer = this.layerManagerMap.get(screenType)
         layerRenderer.addLayerAt(layerName, imageData)
     }
 
+    /**
+     * Changes the color of the layer for a specific screen whose name is given.
+     * @param {string} screenType name of the screen type.
+     * @param {string} layerName name of the image layer.
+     * @param {ImageData} colorImageData imageData of the color to be applied.
+     */
     changeColor(screenType, layerName, colorImageData)
     {
         let layerRenderer = this.layerManagerMap.get(screenType)
         layerRenderer.changeColor(layerName, colorImageData)
     }
 
+    /**
+     * Replaces the texture of the layer for a specific screen whose name is given.
+     * @param {string} screenType name of the screen type.
+     * @param {string} layerName name of the image layer.
+     * @param {ImageData} newImageData imageData of the color to be applied.
+     */
     changeLayer(screenType, layerName, newImageData)
     {
         let layerRenderer = this.layerManagerMap.get(screenType)
         layerRenderer.changeLayer(layerName, newImageData)
     }
     
+    /**
+     * Changes the color of the layer for all screen.
+     * @param {string} layerName name of the image layer.
+     * @param {ImageData} colorImageData imageData of the color to be applied.
+     */
     changeColorForAllScreens(layerName, colorImageData)
     {
         let layerRenderers = this.layerManagerMap.values()
@@ -34,6 +73,11 @@ export class LayerManager
             layerRenderer.changeColor(layerName, colorImageData)
     }
 
+    /**
+     * Replaces the texture of the layer for all screen.
+     * @param {string} layerName name of the image layer.
+     * @param {ImageData} newImageData imageData of the color to be applied.
+     */
     changeLayerForAllScreens(layerName, newImageData)
     {
         let layerRenderers = this.layerManagerMap.values()
@@ -41,6 +85,10 @@ export class LayerManager
             layerRenderer.changeLayer(layerName, newImageData)
     }
 
+    /**
+     * @param {string} screenType name of the screen type.   
+     * @param {Canvas} canvas the canvas onto which the final combine image of all layers of a specific screen type will be rendered.    
+     */
     drawOnCanvas(screenType, canvas)
     {
         let layerRenderer = this.layerManagerMap.get(screenType)
@@ -56,14 +104,27 @@ export class LayerManager
  */
 class LayerRenderer
 {
+    /**
+     * @param {Array} layerNames see above for more info about it.
+     */
     constructor(layerNames)
     {
-        this.finalImageData = null
+        this.finalImageData = null  //contains the final image with all the layers that is ready to be displayed.
         this.layerNames = layerNames
-        this.actualLayersDataMap = new Map()
-        this.visibleLayersDataMap = new Map() 
+        this.actualLayersDataMap = new Map() //the layers whose colors have not been modified
+        this.visibleLayersDataMap = new Map()  //the layers whose colors have been or will be modified.
+        /* 
+        The reason for having two separate maps to store the layers is to allow the individual layers to properly switch between colors in such a way that the presence of the 
+        unselected colors will not be seen when another color is selected.
+        This is only needed in case of colors and will not be needed for the textures. 
+        */
     }
 
+    /**
+     * Adds image layers.
+     * @param {string} layerName name of the image layer.
+     * @param {ImageData} imageData imageData of the image layer that is to be added.
+     */
     addLayerAt(layerName, imageData)
     {
         this.actualLayersDataMap.set(layerName, imageData)
@@ -74,9 +135,14 @@ class LayerRenderer
             this.finalImageData = new ImageData(array, imageData.width, imageData.height)
         }
         if (this.layerNames.length == this.visibleLayersDataMap.size)
-            this.drawLayers()
+            this.combineLayers()
     }
 
+    /**
+     * Changes the color of the layer.
+     * @param {string} layerName name of the image layer.
+     * @param {ImageData} colorImageData imageData of the color to be applied.
+     */
     changeColor(layerName, colorImageData)
     {
         let imageData = duplicateImagedata(this.actualLayersDataMap.get(layerName))
@@ -89,9 +155,14 @@ class LayerRenderer
             imagePixels[i+1] = multiplyColor(imagePixels[i+1], colorPixels[i+1])
             imagePixels[i+2] = multiplyColor(imagePixels[i+2], colorPixels[i+2])
         }
-        this.drawLayers()
+        this.combineLayers()
     }
 
+    /**
+     * Replaces the texture of the layer.
+     * @param {string} layerName name of the image layer.
+     * @param {ImageData} newImageData imageData of the color to be applied.
+     */
     changeLayer(layerName, newImageData)
     {
         let oldImageData = this.actualLayersDataMap.get(layerName)
@@ -100,9 +171,12 @@ class LayerRenderer
         let newPixels = newImageData.data
         for(let i=0; i<oldPixels.length; i++)
             oldPixels[i] = newPixels[i]
-        this.drawLayers()
+        this.combineLayers()
     }
 
+    /**Draws finalImageData into the canvas.
+     * @param {Canvas} canvas the canvas onto which finalImageData is to be displayed.
+     */
     renderToCanvas(canvas)
     {
         if (this.layerNames.length == this.visibleLayersDataMap.size)
@@ -114,7 +188,10 @@ class LayerRenderer
         }
     }
 
-    drawLayers()
+    /**
+     * Combines all the layers and stores them in finalImageData
+     */
+    combineLayers()
     {
         let firstImageData = this.visibleLayersDataMap.get(this.layerNames[0])
         let imageSize = firstImageData.data.length
